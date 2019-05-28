967c8560e7606946ce390a29e10c7ff2fa2f24bc
jeongwoo25 <ljwoo98@naver.com>
2019-05-28 03:25:04 -0700

    [UPDATE] edit BubbleSort

--- sorting/BubbleSort.c
+++ sorting/BubbleSort.c
@@ -13,49 +13,45 @@ void display(int arr[], int n){
     
 }
 
-/*Swap function to swap two values*/
-void swap(int *first, int *second){
-    
-    int temp = *first;
-    *first = *second;
-    *second = temp;
-    
-}
 
 /*This is where the sorting of the array takes place
  arr[] --- Array to be sorted
- size --- Array Size
- */
-void bubbleSort(int arr[], int size){
+ length --- Array Size
+*/
+void bubbleSort(int* arr, int size){
     
+    int tmp;
+
     for(int i=0; i<size-1; i++) {
-        for(int j=0; j<size-1; j++) {
+        for(int j=0; j<size-1-i; j++) {
             if(arr[j]>arr[j+1]) {
-                swap(&arr[j], &arr[j+1]);
+                tmp = arr[j];
+		arr[j] = arr[j+1];
+		arr[j+1] = tmp;
             }
         }
     }
 }
 
-int main(int argc, const char * argv[]) {
-    int n;
-    printf("Enter size of array:\n");
-    scanf("%d", &n); // E.g. 8
+int main(void) {
+    int length;
+    printf("Enter size of array: ");
+    scanf("%d", &length); // E.g. 8
     
-    printf("Enter the elements of the array\n");
+    printf("Enter the elements of the array:\n");
     int i;
-    int arr[n];
-    for(i = 0; i < n; i++){
+    int* arr = (int*)malloc(length * sizeof(int));
+    for(i = 0; i < length; i++){
         scanf("%d", &arr[i] );
     }
     
     printf("Original array: ");
-    display(arr, n);                   // Original array : 10 11 9 8 4 7 3 8
+    display(arr, length);                   // Original array : 10 11 9 8 4 7 3 8
     
-    bubbleSort(arr, n);
+    bubbleSort(arr, length);
     
     printf("Sorted array: ");
-    display(arr, n);                // Sorted array : 3 4 7 8 8 9 10 11
+    display(arr, length);                // Sorted array : 3 4 7 8 8 9 10 11
     
     return 0;
 }
