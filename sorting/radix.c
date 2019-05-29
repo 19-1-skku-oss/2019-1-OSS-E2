2c53f264a19041196a965abc34890b76daa7458d
redjo99 <swiftjo99@naver.com>
2019-05-29 23:54:50 +0900

    radix 5.29

--- sorting/radix.c
+++ sorting/radix.c
@@ -1,8 +1,7 @@
 // This code is for radix sorting
-// redjo99 updated in 28.May.
+// redjo99 updated in 29.May.
 
 #include<stdio.h>
-#include<string.h>
 
 int findmaxdigit(int arr[], int size) {
 	int m = arr[0];
@@ -17,14 +16,46 @@ int findmaxdigit(int arr[], int size) {
 	return numdigits;
 }
 
+int localmin(int arr[], int init, int final) {
+	int m = init;
+	for (int i = init; i < final; i++) {
+		if (arr[m] > arr[i]) m = i;
+	}
+	return m;
+}
+
 void radix(int arr[], int size) {
 	int digit=1;
+	int* copy = malloc(sizeof(int)*size);
+	int* dig=malloc(sizeof(int)*size);
+	int absm, locm;
 	int maxdigit = findmaxdigit(arr, size);
+	for (int i = 0; i < size; i++) {
+		copy[i] = arr[i];
+	}
 	for (int i = 0; i < maxdigit; i++) {
 		for (int j = 0; j < size; j++) {
-
+			dig[j] = copy[j] % 10;
+			copy[j] /= 10;
+		}
+		for (int j = 0; j < size; j++) {
+			absm = j;
+			locm = localmin(dig, j+1, size);
+			if (dig[absm] > dig[locm]) {
+				int temp = dig[absm];
+				dig[absm] = dig[locm];
+				dig[locm] = temp;
+				temp = copy[absm];
+				copy[absm] = copy[locm];
+				copy[locm] = temp;
+				temp = arr[absm];
+				arr[absm] = arr[locm];
+				arr[locm] = temp;
+			}
 		}
 	}
+	free(copy);
+	free(dig);
 }
 
 int main(void) {
