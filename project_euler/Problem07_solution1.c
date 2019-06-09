5fcf831b6868f15ece9dd011d00582ebd8ad5730
redjo99 <49435750+redjo99@users.noreply.github.com>
2019-06-10 00:03:34 +0900

    Update Problem07_solution1.c

--- project_euler/Problem07_solution1.c
+++ project_euler/Problem07_solution1.c
@@ -1,79 +1,31 @@
 // This code is for 1st solution of Project Euler Problem 7.
-// redjo99 updated in 9.June.
+// redjo99 updated in 5.June.
 
 #include<stdio.h>
-#include<math.h>
 
-int primesLessThanN(int arr[], int n) {
-	int cnt = 0;
-	int is_prime;
-	for (int i = 2; i <= n; i++) {
-		is_prime = 1;
-		for (int j = 2; j <= sqrt(i); j++) {
-			if (i%j == 0) {
-				is_prime = 0;
+int findnthprime(int n) {
+	int prime = 1, cnt = 0, isprime;
+	while (cnt < n) {
+		prime++;
+		isprime = 1;
+		for (int i = 2; i < prime; i++) {
+			if (prime%i == 0) {
+				isprime = 0;
 				break;
 			}
 		}
-		if (is_prime) {
-			arr[cnt++] = i;
+		if (isprime == 1) {
+			cnt++;
 		}
 	}
-
-	return cnt--;
-}
-
-void multiplyPrimes(int arr[], int newarr[], int n, int prime_num) {
-	int num, cnt = 0;
-	for (int i = 2; i <= n; i++) {
-		for (int j = 0; j <= prime_num; j++) {
-			newarr[j] = 0;
-			num = i;
-			if (num > 1) {
-				while (num%arr[j] == 0) {
-					num /= arr[j];
-					cnt++;
-				}
-				if (cnt > newarr[j]) {
-					newarr[j] = cnt;
-				}
-			}
-			else {
-				break;
-			}
-		}
-	}
-}
-
-int lastCalculation(int arr[], int newarr[], int prime_num) {
-	int result = 1, sub_result = 1;
-	for (int i = 0; i < prime_num; i++) {
-		for (int j = 0; j < newarr[i]; j++) {
-			sub_result *= arr[i];
-		}
-		result *= sub_result;
-	}
-
-	return result;
+	return prime;
 }
 
 int main(void) {
-	int n, num_primes, result;
-
-	printf("This program finds the smallest positive number that is\n");
-	printf("evenly divisible by all of the numbers from 1 to N\n");
-	printf("Input N: ");
+	int n, prime;
 	scanf("%d", &n);
-	
-	int* primes = malloc(sizeof(int)*n);
-	num_primes = primesLessThanN(primes, n);
-	int* cnt_primes = malloc(sizeof(int)*num_primes);
-	multiplyPrimes(primes,cnt_primes, n, num_primes);
-	result = lastCalculation(primes, cnt_primes, num_primes);
-
-	printf("The result for %d is %d", n, result);
-	free(cnt_primes);
-	free(primes);
+	prime = findnthprime(n);
+	printf("%dth prime number is %d\n", n, prime);
 
 	return 0;
 }
