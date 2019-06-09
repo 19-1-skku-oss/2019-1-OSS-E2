94dc5e3f923dd5f1794ec6caeb1ab6473aa094e4
jeongwoo25 <47218185+jeongwoo25@users.noreply.github.com>
2019-06-09 18:04:52 +0900

    Update recursive_traversals.c

--- data_structures/binary_trees/recursive_traversals.c
+++ data_structures/binary_trees/recursive_traversals.c
@@ -5,30 +5,30 @@
 
 #include <stdio.h>
 
-void inOrderTraversal(struct node *node)
+struct node* inOrderTraversal(struct node *node)
 {
     if(node == NULL) //if tree is empty
-        return;
+        return node;
 
     inOrderTraversal(node->leftNode);
     printf("\t%d\t", node->data);
     inOrderTraversal(node->rightNode);
 }
 
-void preOrderTraversal(struct node *node)
+struct node* preOrderTraversal(struct node *node)
 {
     if(node == NULL) //if tree is empty
-        return;
+        return node;
 
     printf("\t%d\t", node->data);
     preOrderTraversal(node->leftNode);
     preOrderTraversal(node->rightNode);
 }
 
-void postOrderTraversal(struct node *node)
+struct node* postOrderTraversal(struct node *node)
 {
     if(node == NULL) //if tree is empty
-        return;
+        return node;
 
     postOrderTraversal(node->leftNode);
     postOrderTraversal(node->rightNode);
