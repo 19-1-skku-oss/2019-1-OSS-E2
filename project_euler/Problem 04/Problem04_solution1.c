714de084509abbaec82d22dfdf20de6e85644e2d
redjo99 <49435750+redjo99@users.noreply.github.com>
2019-06-10 23:48:07 +0900

    Update Problem04_solution1.c

--- project_euler/Problem 04/Problem04_solution1.c
+++ project_euler/Problem 04/Problem04_solution1.c
@@ -1,20 +1,42 @@
+
 // This code is for 1st solution of Project Euler Problem 4.
-// redjo99 updated in 7.June.
+// redjo99 updated in 10.June.
 
 #include<stdio.h>
 
-int findPalindromicNum(int n) {
+void findPalindromicNum(int n) {
 	int digit = 0;
-	int result;
-	int is_palindromic = 0;
-	while (n > 0) {
-		n /= 10;
+	int is_palindromic;
+	int max = n * n;
+	int number, cnt;
+	while (max > 0) {
+		max /= 10;
 		digit++;
 	}
 	int* digits = malloc(sizeof(int)*digit);
+	for (int i = n; i >= 100; i--) {
+		for (int j = n; j >= 100; j--) {
+			number = i * j;
+			cnt = 0;
+			while (number > 0) {
+				digits[cnt++] = number % 10;
+				number /= 10;
+			}
+			cnt--;
+			is_palindromic = 1;
+			for(int k=0;k<cnt/2;k++) {
+				if (digits[k] != digits[cnt - k]) {
+					is_palindromic = 0;
+					break;
+				}
+			}
+			if (is_palindromic == 1) {
+				printf("%d\n", i*j);
+			}
+		}
+	}
 
 	free(digits);
-	return result;
 }
 
 int main(void) {
@@ -23,8 +45,8 @@ int main(void) {
 	printf("(made from the product of two 3-digit numbers which is less than N)\n");
 	printf("Input N: ");
 	scanf("%d", &n);
-	palindromic_num = findPalindromicNum(n);
-	printf("The result for %d is %d", n, palindromic_num);
+	printf("The palindromic numbers are:\n");
+	findPalindromicNum(n);
 
 	return 0;
 }
